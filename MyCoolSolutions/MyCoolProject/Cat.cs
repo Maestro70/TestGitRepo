ecec4cb21f90ecfb1a00bdbeebeba3cf0b1d7d7c
Maestro70 <32326717+Maestro70@users.noreply.github.com>
2018-04-22 18:32:20 +0300

    It is my new file and my cool project.

---
+++ MyCoolSolutions/MyCoolProject/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoolProject
+{
+    public class Cat
+    {
+
+        public Cat(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MyCoolSolutions/MyCoolProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoolProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Mario");
+            Console.WriteLine("Hello GitHub!!!");
+            Console.WriteLine(cat.Name);
+        }
+    }
+}
