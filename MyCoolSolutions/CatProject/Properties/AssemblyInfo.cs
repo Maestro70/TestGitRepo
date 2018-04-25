c06025c29d92ea3754106e38348e8f8c17c12bc3
Maestro70 <32326717+Maestro70@users.noreply.github.com>
2018-04-25 11:02:10 +0300

    change AssemblyInfo.cs

--- MyCoolSolutions/CatProject/Properties/AssemblyInfo.cs
+++ MyCoolSolutions/CatProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyCoolProject")]
+[assembly: AssemblyTitle("CatProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MyCoolProject")]
+[assembly: AssemblyProduct("CatProject")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
